b441a53813852d934b40d348cf26cd02a6da6299
Omurbek Choibekov <omka3s@gmail.com>
2017-12-15 15:27:55 +0600

    test3

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -8,5 +8,6 @@ driver = :selenium
 
 Capybara.default_driver = driver
 # Capybara.app_host = 'http://cp.kwikichat.com/'
+@browser = Watir::Browser.new :remote, :url=>"http://127.0.0.1:4444/wd/hub", http_client: client, desired_capabilities: caps
 Capybara.app_host = 'http://127.0.0.1:9292/'
 Capybara.page.driver.browser.manage.window.resize_to(1440, 900)
