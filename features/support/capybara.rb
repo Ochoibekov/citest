ac939fd08db77bd87bc73174df287245471d7e27
Omurbek Choibekov <omka3s@gmail.com>
2017-12-15 15:21:59 +0600

    tes2

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -8,5 +8,5 @@ driver = :selenium
 
 Capybara.default_driver = driver
 # Capybara.app_host = 'http://cp.kwikichat.com/'
-Capybara.app_host = 'http://localhost:9292/'
+Capybara.app_host = 'http://127.0.0.1:9292/'
 Capybara.page.driver.browser.manage.window.resize_to(1440, 900)
